29e61bcde3c0a928e238f54a9e9fea3c00099ed7
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-13 13:17:54 +0100

    76. Declare a budgetCalories variable and set it to the result of calling getCaloriesFromInputs – pass an array containing your budgetNumberInput as the argument.

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ function calculateCalories(e) {
   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 }
 
 function getCaloriesFromInputs(list) {
