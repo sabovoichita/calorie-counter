420acf78b9dc3962c1efbb135dac6f5719b3899b
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-13 12:34:51 +0100

    63. Add an if statement that checks if invalidInputMatch is truthy.

--- index.js
+++ index.js
@@ -37,9 +37,12 @@ function addEntry() {
 
 function getCaloriesFromInputs(list) {
   let calories = 0;
+
   for (const item of list) {
     const currVal = cleanInputString(item.value);
     const invalidInputMatch = isInvalidInput(currVal);
+  }
+  if (invalidInputMatch) {
   }
 }
 
