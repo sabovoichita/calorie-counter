886b752a1a6242303d1bd15f76f0338ccdc3247f
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-13 09:29:01 +0100

    18. Start by declaring a cleanInputString function that takes a str parameter.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
 let isError = false;
+
+function cleanInputString(str) {}
+
 console.log(calorieCounter);
 console.log(budgetNumberInput);
 console.log(entryDropdown);
