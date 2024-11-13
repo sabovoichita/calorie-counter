b38ea6e0f0b40aa7b613fdc38a434adf3c680eee
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-13 10:50:03 +0100

    42. Replace your concatenated string in the querySelector with a template literal – be sure to keep the space between your targetId variable and .input-container.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function isInvalidInput(str) {
 function addEntry() {
   const targetId = "#" + entryDropdown.value;
   const targetInputContainer = document.querySelector(
-    targetId + " .input-container"
+    `${targetId} .input-container`
   );
 }
 
