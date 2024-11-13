c1e626dcfe93c0180816a54d9173fe28d6b7fbc9
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-13 17:24:12 +0100

    corrected one error

--- index.js
+++ index.js
@@ -18,21 +18,17 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-  console.log("enterying new entries");
   const targetInputContainer = document.querySelector(
     `#${entryDropdown.value} .input-container`
   );
   const entryNumber =
     targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
-  const HTMLString = `<label for = "${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  let HTMLString = `<label for = "${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
   <input type = "text" placeholder = "Name" id = "${entryDropdown.value}-${entryNumber}-name">
   <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
   <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>
   `;
-  targetInputContainer += targetInputContainer.insertAdjacentHTML(
-    "beforeend",
-    HTMLString
-  );
+  targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
 
 function calculateCalories(e) {
