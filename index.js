e9fa1136f4e2583a15d017fcde0cc5f570a9f8eb
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-13 09:17:40 +0100

    15. Get your #budget element and assign it to budgetNumberInput, and your #entry-dropdown element and assign it to entryDropdown.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
 console.log(calorieCounter);
+console.log(budgetNumberInput);
+console.log(entryDropdown);
