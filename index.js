d7b6684173c5a668d373c7f35b07d40ce3f1654e
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-13 17:05:18 +0100

    90. Your final feature to add is the ability for a user to clear the form. Start by declaring an empty function called clearForm – it should not take any arguments.

--- index.js
+++ index.js
@@ -97,6 +97,8 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
+function clearForm() {}
+
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
 // console.log(calorieCounter);
