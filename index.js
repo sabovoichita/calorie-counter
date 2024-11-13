74270f32add8ce1585d80b5ab6297b01dcf0d336
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-13 09:37:49 +0100

    23. Add the g flag to your regex pattern.const helloRegex = /hello/g;

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const output = document.getElementById("output");
 let isError = false;
 
 function cleanInputString(str) {
-  const regex = /[+-\s]/;
+  const regex = /[+-\s]/g;
 }
 
 console.log(calorieCounter);
