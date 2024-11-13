0a28c3d62be76130369e7766e00fdc03a6458851
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-13 17:12:33 +0100

    93. Create a for...of loop with a variable called container to iterate through the inputContainers array.Inside the loop, set the innerHTML property of the container to an empty string. This will clear all of the contents of that input container.

--- index.js
+++ index.js
@@ -101,6 +101,9 @@ function clearForm() {
   const inputContainers = Array.from(
     document.querySelectorAll(".input-container")
   );
+  for (let container of inputContainers) {
+    container.innerHTML = ``;
+  }
 }
 
 addEntryButton.addEventListener("click", addEntry);
