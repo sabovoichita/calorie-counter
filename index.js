0659d23d085eb376b688e2c7aad19fed145afadb
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-13 10:52:52 +0100

    43. Thanks to template literals, you actually don't need the targetId variable at all. Remove that variable, and update your template literal to replace targetId with entryDropdown.value – remember to add # before that, in the string.

--- index.js
+++ index.js
@@ -18,9 +18,8 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-  const targetId = "#" + entryDropdown.value;
   const targetInputContainer = document.querySelector(
-    `${targetId} .input-container`
+    `#${entryDropdown.value} .input-container`
   );
 }
 
